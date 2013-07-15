7f81da71243fadd011e04a0008afa030f58f8229
Lior Kirsch <lior@lior.(none)>
2013-07-15 18:59:41 +0300

    added correlation using ontology distances

--- @ontology/ontology.m
+++ @ontology/ontology.m
@@ -7,6 +7,10 @@ classdef ontology
         recursiveStructure;
         directedDistanceMatrix;
         unDirectedDistanceMatrix;
+        closestCommonParentIndex;
+        meanDistanceToParent;
+        shortDistanceToParent;
+        longDistanceToParent;
     end
     
     methods
@@ -48,6 +52,7 @@ classdef ontology
             [~,indexInOntlogy] = ismember(regionNames, obj.structureLabels(:,4));
             regionColors = colors(indexInOntlogy,:); 
         end
+        
         function childOfNodes = getIndexesOfChilds(obj,parentNodeIndexes)
             [allChilds, ~] = obj.allChildNodes();
             childOfNodes = allChilds(parentNodeIndexes, :);
@@ -59,6 +64,76 @@ classdef ontology
             nodeLevel = sum(allChilds,1);
         end
         
+        function directedDistences = getDistances(obj)
+            X = obj.dependencyMatrix;
+            directedDistences = ( inv(eye(size(X)) - X) )^2*X;
+            directedDistences(directedDistences ==0) = inf;
+            directedDistences(logical(eye(size(X)))) = 0;
+        end
+        
+        
+        function obj = distanceToCommonParent2(obj)
+            [allChilds, nodeLevel] = obj.allChildNodes();
+            nodeLevel = nodeLevel';
+            distanceToParent = zeros(size(obj.dependencyMatrix));
+            obj.closestCommonParentIndex = zeros(size(obj.dependencyMatrix));
+            numberOfNodes = size(obj.dependencyMatrix,1);
+            
+            for i = 1: (numberOfNodes -1)
+                ancestorOf_i_j = repmat(allChilds(:,i),1, numberOfNodes ) & allChilds;
+                
+                nodeReplicated = repmat(nodeLevel,1, numberOfNodes ) ;
+                parentLevel = nodeReplicated .* ancestorOf_i_j;
+                        
+                [~, bestParentIndex] = max(parentLevel, [], 1 );
+                distanceToParent(:,i) = obj.unDirectedDistanceMatrix(i,bestParentIndex);
+                obj.closestCommonParentIndex(:,i) = bestParentIndex;
+            end
+            
+            obj.longDistanceToParent =  max(distanceToParent, distanceToParent');
+            obj.shortDistanceToParent =  min(distanceToParent, distanceToParent');
+            obj.meanDistanceToParent = (distanceToParent + distanceToParent')/2;
+        end
+        
+        function [ closestCommonParentIndex, meanDistanceToParent, shortDistanceToParent, longDistanceToParent] = distanceToCommonParent(obj)
+            [allChilds, nodeLevel] = obj.allChildNodes();
+
+            
+            closestCommonParentIndex = zeros(size(obj.dependencyMatrix));
+            meanDistanceToParent = zeros(size(obj.dependencyMatrix));
+            shortDistanceToParent = zeros(size(obj.dependencyMatrix));
+            longDistanceToParent = zeros(size(obj.dependencyMatrix));
+
+            numberOfNodes = size(obj.dependencyMatrix,1);
+            
+            
+            for i = 1: numberOfNodes
+                for j= i+1 : numberOfNodes
+                    ancestorOf_i_j = [ allChilds(:,i) , allChilds(:,j)];
+                    ancestorOf_i_j = all( ancestorOf_i_j,2);
+                    ancestorsIndices = find(ancestorOf_i_j);
+                    
+                    [~, bestParentmInIndex] = max(nodeLevel (ancestorOf_i_j) );
+                    bestParentmInIndex = ancestorsIndices(bestParentmInIndex);
+                    
+                    parentDistanceFrom_i = obj.directedDistanceMatrix(i,bestParentmInIndex);
+                    parentDistanceFrom_j = obj.directedDistanceMatrix(j,bestParentmInIndex);
+                    distances = [parentDistanceFrom_i, parentDistanceFrom_j];
+                    sortedDist = sort(distances);
+                    shortDistanceToParent(i,j) = sortedDist(1);
+                    longDistanceToParent(i,j) = sortedDist(2);
+                    meanDistanceToParent(i,j) = mean(distances);
+                    closestCommonParentIndex(i,j) = bestParentmInIndex;
+                    
+                end
+            end
+            
+            closestCommonParentIndex = closestCommonParentIndex + closestCommonParentIndex';
+            meanDistanceToParent = meanDistanceToParent + meanDistanceToParent';
+            shortDistanceToParent = shortDistanceToParent + shortDistanceToParent';
+            longDistanceToParent = longDistanceToParent + longDistanceToParent';
+            
+        end
         
         function reducedOntology = reduceToLeafAndParents(obj,leafIndices)
 
--- mainDistances.m
+++ mainDistances.m
@@ -1,38 +1,39 @@
 function mainDistances()
-%     load('pcaData.mat', 'experimentRegion', 'experimentsDataMatrix','experimentsLocationMatrix','experimentsSubjectMatrix','regionNames','mni_xyz', 'mri_voxel_xyz');
+
+%     load('easyFormatHumanData.mat');
+% 
 %     %distanceMatrixBetweenSamples = pdist2(experimentsDataMatrix,experimentsDataMatrix,'euclidean');
-%     
+% 
 %     expressionDistances = pdistAlternative(experimentsDataMatrix);
 %     mniDistances = pdistAlternative(mni_xyz);
 %     mriDistances = pdistAlternative(mri_voxel_xyz);
-%     
+%       
 %     save('dataWithDistance.mat');
 
     load('dataWithDistance.mat');
-    
-    
     load('humanOntologyObject.mat');
     load('subset.mat');
 
     numOfSamples = sum(experimentsLocationMatrix  ,1)';
-    aboveK = numOfSamples >= 10;
-    
-    subsetNodesIndex = subsetNodesIndex & aboveK;
-    %subsetNodesIndex = true(size(aboveK));
+   
     regionColors = humanOntology.structureColors(subsetNodesIndex,:);
     regionNames = humanOntology.structureLabels(subsetNodesIndex,4);
 
 
 
-    numberOfPeople = 6;
-    numberOfRegions = size(experimentRegion,2);
+    numberOfPeople = size(experimentsSubjectMatrixLogical,2);
+    numberOfRegions = size(experimentsLocationMatrix,2);
     
-    experimentsSubjectMatrixLogical = false(size(experimentsSubjectMatrix,1), numberOfPeople);
-    for i =1:numberOfPeople
-        experimentsSubjectMatrixLogical(:,i) = experimentsSubjectMatrix == i;
-    end
+    [shortOntlogyDistances, longOntlogyDistances, meanDistances] = clacOntologyDistances(experimentsLocationMatrix, humanOntology);
+    scatterDistances(expressionDistances, meanDistances, experimentsSubjectMatrixLogical, 'mean of the two distances to ontology common parent');
+    scatterDistances(expressionDistances,  longOntlogyDistances, experimentsSubjectMatrixLogical, 'long distance to ontology common parent');
+    scatterDistances(expressionDistances, shortOntlogyDistances, experimentsSubjectMatrixLogical, 'short distance to ontology common parent');
     
-    scatterDistances(expressionDistances, mniDistances , mriDistances, experimentsSubjectMatrixLogical);
+    
+    scatterDistances(expressionDistances, mriDistances, experimentsSubjectMatrixLogical,'MRI based distance');
+    scatterDistances(expressionDistances, mniDistances , experimentsSubjectMatrixLogical,'MNI based distance');
+    
+    
     
     regionandPeoplePrecision = nan(numberOfRegions,1);
     regionandPeoplePrecisionSTD = nan(numberOfRegions,1);
@@ -66,6 +67,22 @@ function mainDistances()
     plotDistances(experimentRegion, expressionDistances);
 end
 
+function [shortDistances, longDistances, meanDistances] = clacOntologyDistances(experimentsLocationMatrix, ontologyObj)
+    numberOfRegions = size(experimentsLocationMatrix,2);
+    numberOfSamples = size(experimentsLocationMatrix,1);
+    
+    shortDistances = nan(numberOfSamples);
+    longDistances = nan(numberOfSamples);
+    
+    experimentRegionIndex = experimentsLocationMatrix * (1:size(experimentsLocationMatrix,2))';
+    for i =1:numberOfSamples
+        iIndex = experimentRegionIndex(i);
+        shortDistances(i,:) = ontologyObj.shortDistanceToParent(iIndex, experimentRegionIndex);
+        longDistances(i,:) = ontologyObj.longDistanceToParent(iIndex, experimentRegionIndex);
+    end
+    
+    meanDistances = (shortDistances + longDistances) /2 ;
+end
 function plotDistances(experimentRegion, distanceMatrixBetweenSamples)
     experimentRegionIndex = experimentRegion * (1:size(experimentRegion,2))';
     [sortedValues,sortByRegion] = sort(experimentRegionIndex);
@@ -81,26 +98,32 @@ end
 
 function distances = pdistAlternative(x)
     Qx=repmat(dot(x,x,2),1,size(x,1));
-    distances = sqrt(Qx+Qx'-2*(x*x'));
+    distances = Qx+Qx'-2*(x*x');
+
+    distances( logical(eye(size(distances)) )) = 0;
+    distances = sqrt(distances);
 end
 
-function scatterDistances(expressionDistances, mniDistances , mriDistances, experimentsSubjectMatrixLogical)
+function scatterDistances(expressionDistances, distances, experimentsSubjectMatrixLogical, label)
     numberOfSubjects = size(experimentsSubjectMatrixLogical,2);
-    for i = 1:numberOfSubjects
-        releventExp = experimentsSubjectMatrixLogical(:,i);
-        releventExpressionDistances = expressionDistances(releventExp,releventExp);
-        releventMRIDistances = mriDistances(releventExp,releventExp);
-        releventMNIDistances = mniDistances(releventExp,releventExp);
-        
-        figure(i*10);
-        scatter(releventExpressionDistances(:), releventMNIDistances(:),'.');
-        xlabel('gene expression based distance');
-        ylabel('MNI based distance');
-
-        figure(i*20);
-        scatter(releventExpressionDistances(:), releventMRIDistances(:),'.');
-        xlabel('gene expression based distance');
-        ylabel('MRI based distance');
-    end
+    randI = randi(10000);
+    
+%     for i = 1:numberOfSubjects
+%         releventExp = experimentsSubjectMatrixLogical(:,i);
+%         releventExpressionDistances = expressionDistances(releventExp,releventExp);
+%         releventDistances = distances(releventExp,releventExp);
+%         pearsonCorr = corr(releventExpressionDistances(:),releventDistances(:),'type','Pearson');
+%         spearmanCorr = corr(releventExpressionDistances(:),releventDistances(:),'type','Spearman');
+%         
+%         figure(randI + i);
+%         scatter(releventExpressionDistances(:), releventDistances(:),'.');
+%         xlabel('gene expression based distance');
+%         ylabel(label);
+%         title(sprintf('correlation: %g pearson, %g spearman', pearsonCorr, spearmanCorr));
+%         
+%     end
+    
+    [spearmanCorr, pval] = corr(expressionDistances(:),distances(:),'type','Spearman');
+    fprintf('%s \t %g (pval %g)\n', label, spearmanCorr, pval);
 end
     
